7086061b0a9672023f99ac27ee2e904d1d261186
PS-410 <119973129+PS-410@users.noreply.github.com>
2022-12-09 11:32:36 +0100

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Paul Schmidt - Analyst**
+
+# *I am interested in data science because I think it will be very relevant fopr my job in the future*
+
+# ### My code in the next line will solve the equation 1+1
+
+# In[6]:
+
+
+1+1
+
+
+# - Bullet list 1
+# - Bullet list 2
+# 
+# [Link](https://www.google.com)
+# 
+# 1. First ordered list item
+# 2. Another item
+
+# In[ ]:
+
+
+
+
